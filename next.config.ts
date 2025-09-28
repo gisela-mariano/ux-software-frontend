6a48e1a014772ba77281636a2947bc9e0ff2ac4d
Gisela Mariano <gisela-mariano@hotmail.com>
2025-09-28 11:21:07 -0300

    chore(app): add project configuration files
    - Adds editor configuration, Prettier, and PostCSS, and updates dependencies in package.json and package-lock.json
    - Modifies next.config.ts to include internationalization support and adjusts tsconfig.json for new import paths.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["localhost"],
+  },
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+export default withNextIntl(nextConfig);
