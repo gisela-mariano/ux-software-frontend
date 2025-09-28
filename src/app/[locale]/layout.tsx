7d73cf06b022816c75756a17184133ac906585eb
Gisela Mariano <gisela-mariano@hotmail.com>
2025-09-28 14:02:30 -0300

    refactor(app): simplifies layout and adjusts internationalization logic
    - Removes locale verification logic from RootLayout.
    - Updates LocaleLayout to use parameter promises and verify locale before rendering.
    - Maintains layout structure with internationalization support.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,15 +1,23 @@
 import { Header } from "@/components/atoms/header";
+import { routing } from "@/i18n/routing";
 import { SessionProvider } from "@/providers/sessionProvider";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
 import { PrimeReactProvider } from "primereact/api";
 
 type Props = {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 };
 
-export default async function LocaleLayout({ children }: Readonly<Props>) {
+export default async function LocaleLayout({ children, params }: Readonly<Props>) {
+  const { locale } = await params;
+
+  if (!routing.locales.includes(locale as "pt" | "en")) {
+    notFound();
+  }
+
   const messages = await getMessages();
 
   return (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,14 @@
-import { routing } from "@/i18n/routing";
-import { notFound } from "next/navigation";
 import "primeicons/primeicons.css";
 import { ReactNode } from "react";
 import "./globals.css";
 
 type Props = {
   children: ReactNode;
-  params?: { locale?: "pt" | "en" };
 };
 
-export default function RootLayout({ children, params }: Props) {
-  const locale = params?.locale ?? "pt";
-
-  if (!routing.locales.includes(locale)) {
-    notFound();
-  }
-
+export default function RootLayout({ children }: Props) {
   return (
-    <html lang={locale}>
+    <html lang="pt">
       <body>{children}</body>
     </html>
   );
